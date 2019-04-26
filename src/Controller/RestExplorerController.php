29e18677e8a4f26cb675a2d710837209096356a7
Lorenzo Gandino <lorenzoga94@gmail.com>
2019-04-26 23:27:19 +0200

    Creato servizio getByUrl

--- src/Controller/RestExplorerController.php
+++ src/Controller/RestExplorerController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+
 
 use App\Service\StubRepoManager;
 
@@ -16,5 +18,22 @@ class RestExplorerController {
       ];
     }
     return new JsonResponse($stubList);
+  }
+  
+  public function getByUrl(RequestStack $requestStack, StubRepoManager $stubManager) {
+    $stub = $stubManager -> getStubRepo() -> getByUrl($requestStack -> getCurrentRequest() -> query -> get('url'));
+    
+    if($stub) {
+      $ret = [
+        'file' => $stub -> getFile(),
+        'url' => $stub -> getUrl(),
+        'response' => $stub -> getResponse()
+      ];
+    } else {
+      $ret = [
+        'error' => 'Stub not found'
+      ];
+    }
+    return new JsonResponse($ret);
   }
 }
